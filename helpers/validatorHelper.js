859eb917714d46d1b54913db77a724cbeffa80f4
Aimen <aimenbatool72@gmail.com>
2018-05-10 02:32:21 +0500

    Improved error messages.

--- helpers/validatorHelper.js
+++ helpers/validatorHelper.js
@@ -4,7 +4,7 @@ import { matchedData, sanitize } from "express-validator/filter";
 export const addUniValidator = [
   check('universityName', 'University name can not be empty.').isLength({min : 1}).withMessage('Name is required.'),
   check('address', 'Address is required.').isLength({min : 1 }).isLength({max : 10}).withMessage('Max length is 10.'),
-  check('city', 'City iiiis required').isLength({min : 1}),
-  check('country', 'iiiCountry is required.').isLength({min : 1}),
-  check('contact', 'Contact iiiis required.').isLength({min : 1})
+  check('city', 'City is required').isLength({min : 1}),
+  check('country', 'Country is required.').isLength({min : 1}),
+  check('contact', 'Contact is required.').isLength({min : 1})
 ];
