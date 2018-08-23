b446b9a670de181e777b3699bebeeb1a6c909512
Aimen <aimenbatool72@gmail.com>
2018-08-23 12:22:07 +0500

    Add Update University & Update Programs, fix routes, fix depreciation warnings after mongoose update

--- app.js
+++ app.js
@@ -1,73 +1,85 @@
-import path from 'path';
+import path from "path";
 import express from "express";
-import exphbs from 'express-handlebars';
-import flash from 'connect-flash';
-import cookieParser from 'cookie-parser';
-import session from 'express-session';
-import Handlebars from 'handlebars';
-import paginate from 'express-paginate';
-import fileUpload from 'express-fileupload';
+import exphbs from "express-handlebars";
+import flash from "connect-flash";
+import cookieParser from "cookie-parser";
+import session from "express-session";
+import Handlebars from "handlebars";
+import paginate from "express-paginate";
+import fileUpload from "express-fileupload";
 const app = express();
 
 import bodyParser from "body-parser";
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 import route from "./routes/";
 
-const Promise = global.Promise;
+mongoose.connect(
+  "mongodb://localhost:27017/university-app",
+  {
+    useNewUrlParser: true
+  }
+);
 
-mongoose.connect("mongodb://localhost/university-app");
+mongoose.Promise = global.Promise;
 
 // Helper function to increment index value for foreach loop.
-Handlebars.registerHelper("inc", function (value, options) {
+Handlebars.registerHelper("inc", function(value, options) {
   return parseInt(value) + 1;
 });
 
-Handlebars.registerHelper('if_equal', function (a, b, opts) {
+Handlebars.registerHelper("if_equal", function(a, b, opts) {
   if (a == b) {
-    return opts.fn(this)
+    return opts.fn(this);
   } else {
-    return opts.inverse(this)
+    return opts.inverse(this);
   }
 });
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-  extended: false
-}))
+app.use(
+  bodyParser.urlencoded({
+    extended: false
+  })
+);
 app.use(fileUpload());
 
 app.use(cookieParser());
-app.use(session({
-  cookie: {
-    maxAge: 3600000000
-  },
-  secret: "my_secret",
-  saveUninitialized: false,
-  resave: false
-}))
+app.use(
+  session({
+    cookie: {
+      maxAge: 3600000000
+    },
+    secret: "my_secret",
+    saveUninitialized: false,
+    resave: false
+  })
+);
 
 app.use(flash());
 app.use((req, res, next) => {
-  res.locals.success_message = req.flash('success');
-  res.locals.error_message = req.flash('error');
-  res.locals.detailed_message = req.flash('detailed_msg')
+  res.locals.success_message = req.flash("success");
+  res.locals.error_message = req.flash("error");
+  res.locals.detailed_message = req.flash("detailed_msg");
   res.locals.session = req.session.user;
   next();
 });
 
-app.use(paginate.middleware(10, 50))
+app.use(paginate.middleware(10, 50));
 
-app.use("/", route)
+app.use("/", route);
 
-app.engine('handlebars', exphbs({
-  defaultLayout: 'main.handlebars',
-  layoutsDir  : __dirname + '/views/layouts',
-  partialsDir : __dirname + '/views/partials/includes',
-}));
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'handlebars');
+app.engine(
+  "handlebars",
+  exphbs({
+    defaultLayout: "main.handlebars",
+    layoutsDir: __dirname + "/views/layouts",
+    partialsDir: __dirname + "/views/partials/includes"
+  })
+);
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "handlebars");
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 const port = process.env.port || 3000;
 app.listen(3000, () => console.log(`App is running at ${port}`));
--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,11 +1,17 @@
-
 import User from './../models/User';
 import flash from 'connect-flash';
 import paginate from 'express-paginate';
-import { sendWelcomeEmail } from '../services/emailService';
+import {
+  sendWelcomeEmail
+} from '../services/emailService';
 
-import { check , validationResult } from "express-validator/check";
-import { matchedData } from "express-validator/filter";
+import {
+  check,
+  validationResult
+} from "express-validator/check";
+import {
+  matchedData
+} from "express-validator/filter";
 
 import nodemailer from 'nodemailer';
 
@@ -13,9 +19,8 @@ import crypto from 'crypto';
 
 const usersController = {}
 
-usersController.register = (req , res) => {
-  if(req.session.user) {
-    console.log('here')
+usersController.register = (req, res) => {
+  if (req.session.user) {
     return res.redirect('/admin/dashboard')
   }
   res.render('pages/user/signup')
@@ -24,38 +29,51 @@ usersController.register = (req , res) => {
 
 usersController.signup = async (req, res) => {
   const errors = validationResult(req);
-  if(!errors.isEmpty()) {
+  if (!errors.isEmpty()) {
     const user = matchedData(req);
-    return res.render('pages/user/signup' , { errors : errors.mapped() , users : user})
-  }else {
+    return res.render('pages/user/signup', {
+      errors: errors.mapped(),
+      users: user
+    })
+  } else {
     try {
-      const email = await User.findOne({email : req.body.email});
-      if(email){
-        return res.render('pages/user/signup' , {users : req.body , emailError : 'Email is already registered.'})
+      const email = await User.findOne({
+        email: req.body.email
+      });
+      if (email) {
+        return res.render('pages/user/signup', {
+          users: req.body,
+          emailError: 'Email is already registered.'
+        })
       }
-      const username = await User.findOne({username : req.body.username});
-      if(username) {
-        return res.render('pages/user/signup' , {users : req.body , usernameError : 'Username is already registered.'})
+      const username = await User.findOne({
+        username: req.body.username
+      });
+      if (username) {
+        return res.render('pages/user/signup', {
+          users: req.body,
+          usernameError: 'Username is already registered.'
+        })
       }
       let verificationCode = crypto.randomBytes(10).toString('hex');
       const newUser = new User(req.body);
       newUser.verificationCode = verificationCode;
       newUser.isVerified = false;
-      
-      const user = await newUser.save(); 
-      sendWelcomeEmail(req.body.email , verificationCode);
-      req.flash('success' , 'Please, Verify your email to continue.')
-      
-    }catch(error) {
+
+      const user = await newUser.save();
+      sendWelcomeEmail(req.body.email, verificationCode);
+      req.flash('success', 'Please, Verify your email to continue.')
+
+    } catch (error) {
       throw error;
-      req.flash('error' , error.message)
-    }    
+      req.flash('error', error.message)
+    }
     res.redirect('/admin/signin');
   }
 }
 
 usersController.signin = async (req, res) => {
-  if(req.session.user) {
+  if (req.session.user) {
     return res.redirect('/admin/dashboard')
   }
   // res.render('signin');
@@ -64,105 +82,123 @@ usersController.signin = async (req, res) => {
 
 usersController.login = async (req, res) => {
   const errors = validationResult(req);
-  if(!errors.isEmpty()) {
+  if (!errors.isEmpty()) {
     const user = matchedData(req);
-    return res.render('signin', { errors : errors.mapped() , users:user })
-  }else {
+    return res.render('signin', {
+      errors: errors.mapped(),
+      users: user
+    })
+  } else {
     try {
-      const user = await User.findOne({"username" : req.body.username});
-      if( !user ){
-        req.flash('error' , 'User doest not exist.')
-      }else {
+      const user = await User.findOne({
+        "username": req.body.username
+      });
+      if (!user) {
+        req.flash('error', 'User doest not exist.')
+      } else {
         const isMatch = await user.isValidPassword(req.body.password);
-        if( !isMatch ){
+        if (!isMatch) {
           req.flash('error', 'Password doest not match.');
-        }else {
+        } else {
           // if user is active then login else send link to activate.
-          if(user.isVerified == false) {
-            sendWelcomeEmail(user.email , user.verificationCode );
-            req.flash('error' , 'Please check your email and activate your account.')
+          if (user.isVerified == false) {
+            sendWelcomeEmail(user.email, user.verificationCode);
+            req.flash('error', 'Please check your email and activate your account.')
             return res.redirect('/admin/signin')
           }
           // password matched, set session and redirect.
           req.session.user = user;
-          return res.redirect('/admin/dashboard')        
+          return res.redirect('/admin/dashboard')
         }
       }
       res.redirect('/admin/signin')
-    }catch( error){
+    } catch (error) {
       throw Error(error);
     }
   }
-  }
+}
 
-  usersController.home = async(req, res) => {
-    res.render('home')
-  }
+usersController.home = async (req, res) => {
+  res.render('home')
+}
 
-  usersController.signout = async(req, res) => {
-     req.session.destroy((err) => {
-     res.redirect('/admin/signin')
+usersController.signout = async (req, res) => {
+  req.session.destroy((err) => {
+    res.redirect('/admin/signin')
+  });
+}
+
+usersController.verifyAccount = async (req, res) => {
+  let {
+    verificationCode
+  } = req.params;
+  let user = await User.findOneAndUpdate({
+    verificationCode: verificationCode
+  }, {
+    isVerified: true
+  });
+  if (user) {
+    req.flash('success', 'Account activated, You may login now.')
+    res.redirect('/admin/signin')
+  } else {
+    req.flash('error', 'Invalid access.');
+    res.redirect('/admin/signin')
+  }
+}
+
+usersController.forgotPassword = async (req, res) => {
+  res.render('pages/user/forgotpassword')
+
+}
+
+usersController.updatePassword = async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    const user = matchedData(req);
+    return res.render('pages/user/forgotpassword', {
+      errors: errors.mapped(),
+      users: user
     });
-  }
-
-  usersController.verifyAccount = async(req, res) => {
-    let { verificationCode } = req.params;
-    let user = await User.findOneAndUpdate({ verificationCode : verificationCode } , {isVerified : true});
-    if( user ) {
-      req.flash('success', 'Account activated, You may login now.')
-      res.redirect('/admin/signin')
-    }else {
-      req.flash('error', 'Invalid access.');
-      res.redirect('/admin/signin')
-    }
-  }
-
-  usersController.forgotPassword = async (req, res) => {
-    res.render('pages/user/forgotpassword' )
-
-  }
-
-  usersController.updatePassword = async (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      const user = matchedData(req);
-      return res.render('pages/user/forgotpassword' , {errors : errors.mapped() , users: user});
-    }else {
-      let username = req.body.username;
-      let password = req.body.password;
-      const user = await User.findOne({username});
-      if( user ) {
-        user.password = password;
-        user.save();
-        req.flash('success' , 'Password has been updated. Please, login to continue.');  
-      }else {
-        req.flash('error' , 'User doesnt exist.');
-      }
-      res.redirect('/admin/forgotpassword')
-    }
-  }
-
-  usersController.getAllUsers = async (req, res) => {
-    const [ results , itemCount ] = await Promise.all([
-      User.find({}).limit(req.query.limit).skip(req.skip).lean().exec(),
-      User.count({})
-    ])
-    const pageCount = Math.ceil(itemCount / req.query.limit);
-    res.render('pages/user/users', {
-      users: results,
-      pages: paginate.getArrayPages(req)(3, pageCount, req.query.page)
+  } else {
+    let username = req.body.username;
+    let password = req.body.password;
+    const user = await User.findOne({
+      username
     });
+    if (user) {
+      user.password = password;
+      user.save();
+      req.flash('success', 'Password has been updated. Please, login to continue.');
+    } else {
+      req.flash('error', 'User doesnt exist.');
+    }
+    res.redirect('/admin/forgotpassword')
   }
+}
+
+usersController.getAllUsers = async (req, res) => {
+  const [results, itemCount] = await Promise.all([
+    User.find({}).limit(req.query.limit).skip(req.skip).lean().exec(),
+    User.countDocuments({})
+  ])
+  const pageCount = Math.ceil(itemCount / req.query.limit);
+  res.render('pages/user/users', {
+    users: results,
+    pages: paginate.getArrayPages(req)(3, pageCount, req.query.page)
+  });
+}
 
 
-  usersController.deleteUser = async (req , res) => {
-    try {
-      let { userId } = req.params;
-      await User.findByIdAndRemove(userId);
-      req.flash('success' , 'Record is successfully deleted.');
-      res.redirect('/admin/user/manage');
-    }catch(e){
-      throw e;
-    }    
+usersController.deleteUser = async (req, res) => {
+  try {
+    let {
+      userId
+    } = req.params;
+    await User.findByIdAndRemove(userId);
+    req.flash('success', 'Record is successfully deleted.');
+    res.redirect('/admin/user/manage');
+  } catch (e) {
+    throw e;
   }
+}
 export default usersController;
